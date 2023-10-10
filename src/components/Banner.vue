c3dfcb1d6d8de5c7f3a43725588c808f5f238837
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-10-10 19:41:16 -0300

    teste

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,73 +1,78 @@
 <template>
-    <div class="container">
-      <div class="content">
-        <div class="text">
-          <div class="title">ADOPETS</div>
-          <p class="subtitle">Adopets é a sua plataforma online confiável para encontrar o seu novo melhor amigo de quatro patas. Nós conectamos animais adoráveis com famílias amorosas em todo o mundo. Estamos comprometidos em garantir que cada animal encontre um lar amoroso e permanente.</p>
-          <div class="buttons">
-            <div class="btn btn1"><a href="#">Botão 1</a></div>
-            <div class="btn btn2"><a href="#">Botão 2</a></div>
-          </div>
+  <div class="container">
+    <div class="content">
+      <div class="text">
+        <div class="title">ADOPETS</div>
+        <p class="subtitle">
+          Adopets é a sua plataforma online confiável para encontrar o seu novo
+          melhor amigo de quatro patas. Nós conectamos animais adoráveis com
+          famílias amorosas em todo o mundo. Estamos comprometidos em garantir
+          que cada animal encontre um lar amoroso e permanente.
+        </p>
+        <div class="buttons">
+          <div class="btn btn1"><a href="#">Botão 1</a></div>
+          <div class="btn btn2"><a href="#">Botão 2</a></div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
-  export default{
-    name: 'Banner',
-  }
+export default {
+  name: "Banner",
+};
 </script>
 
 <style lang="scss" scoped>
-.container{
-  background-image: url('../assets/img/modelo-banner.png');
+.container {
+  background-image: url("src/assets/img/modelo-banner.png");
   width: 100%;
   height: 800px;
   background-size: cover;
   background-position: center;
   display: flex;
-  .content{
+  .content {
     display: flex;
-    align-items:center;
-    .text{
+    align-items: center;
+    .text {
       width: 50%;
       height: 100%;
       padding: 1rem;
       display: grid;
       gap: 2rem;
       align-content: center;
-      .title{
+      .title {
         font-size: 46px;
         line-height: 25px;
       }
-      .subtitle{
+      .subtitle {
         font: 600 var(--subtitle-font-size) var(--body-font);
       }
-      .buttons{
+      .buttons {
         width: 100%;
         display: flex;
         gap: 1rem;
         align-items: center;
-        .btn1{
+        .btn1 {
           background-color: transparent;
-          a{
+          a {
             color: var(--color-4);
           }
-          &:hover{
+          &:hover {
             border: 3px solid var(--color-3);
             background-color: var(--color-4);
-            a{
+            a {
               color: var(--color-3);
             }
           }
         }
-        .btn2{
+        .btn2 {
           border: 3px solid var(--color-2);
           background-color: var(--color-2);
-          &:hover{
+          &:hover {
             background-color: transparent;
             border: 3px solid var(--color-4);
-            a{
+            a {
               color: var(--color-4);
             }
           }

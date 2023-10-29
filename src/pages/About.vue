a3503e03f35e9c6e7cde7249905948052358d48a
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-10-29 19:15:25 -0300

    ajustes da navbar responsiva e da página about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,2 +1,14 @@
-<template></template>
-<script></script>
+<template>
+  <div class="content">
+    <Navbar />
+    <h1>Página About</h1>
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>

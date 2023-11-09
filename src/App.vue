99f0af1173a73166c56485ae1382fb8bdd9580ef
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-11-09 20:54:36 -0300

    criação do componente footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <p>componente de footer</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  background-color: red;
+  .content {
+    display: flex;
+    justify-content: center;
+    background-color: pink;
+  }
+}
+</style>

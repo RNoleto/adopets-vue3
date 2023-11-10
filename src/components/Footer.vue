288725e3aa836f5aa68db8a09e46401bf9bffd9b
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-11-09 21:44:43 -0300

    ajuste de elementos do footer mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="container">
     <div class="content">
-      <p>componente de footer</p>
+      <router-link class="link" to="/">
+        <img src="../assets/img/logo.png" alt="logo" class="logo" />
+        <h1 class="title">Adopets</h1>
+      </router-link>
+      <div class="socials">
+        <p>Facebook</p>
+        <p>Instagram</p>
+        <p>Whatsapp</p>
+        <p>TikTok</p>
+      </div>
+      <div class="copyright">
+        <p>Todos os direitos reservados.</p>
+        <p>Desenvolvido por Ramon Ribeiro Noleto</p>
+        <p>devnoleto@gmail.com</p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +27,44 @@ export default {
 </script>
 <style lang="scss" scoped>
 .container {
+  padding: 3rem 0rem;
   background-color: red;
   .content {
+    padding: 0px 10px;
     display: flex;
-    justify-content: center;
-    background-color: pink;
+    gap: 1rem;
+    flex-wrap: wrap;
+    .link {
+      width: 100%;
+      display: flex;
+      gap: 0.2rem;
+      align-items: center;
+      justify-content: center;
+      .logo {
+        width: 60px;
+      }
+      .title {
+        font-size: 24px;
+      }
+    }
+    .socials {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+      align-items: center;
+      justify-content: center;
+    }
+    .copyright {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      font-size: 12px;
+      gap: 0.1rem;
+    }
   }
 }
 </style>

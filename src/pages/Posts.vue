518cf83a0f928f86e6e6b52d2873b93c02f47721
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-11-27 20:57:51 -0300

    ajuste do consumo do arquivo json de posts

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,14 +1,43 @@
 <template>
   <Navbar />
-  <h1>Página de Posts</h1>
+  <div class="container">
+    <div class="content page">
+      <h1 class="title">Página de Posts</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-import Navbar from "../components/Navbar.vue";
+import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
 export default {
   name: "Posts",
   components: {
     Navbar,
   },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("src/api/adoption.json")
+      .then((response) => {
+        this.posts = response.data.posts;
+        console.log("Dados carregado do Json:", this.posts);
+      })
+      .catch((error) => {
+        console.log("Erro ao carregar posts:", error);
+      });
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  .content {
+    padding: 0px 10px;
+  }
+}
+</style>

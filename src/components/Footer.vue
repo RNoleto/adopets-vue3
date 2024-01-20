f20bf7289070ff798c628a6437dd7f45d4cbe5af
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-01-20 18:52:45 -0300

    main
    Ajuste chamada de footer e estilo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,186 +1,68 @@
 <template>
-  <nav>
-    <div class="adopets">
+  <div class="container">
+    <div class="content">
       <router-link class="link" to="/">
         <img src="assets/img/logo.png" alt="logo" class="logo" />
         <h1 class="title">Adopets</h1>
       </router-link>
+      <Socials />
+      <div class="copyright">
+        <p>Todos os direitos reservados.</p>
+        <p>Desenvolvido por Ramon Ribeiro Noleto</p>
+        <p>devnoleto@gmail.com</p>
+      </div>
     </div>
-    <!-- Menu Mobile -->
-    <div class="navbar-toggle" @click="toggleNav">
-      <div class="bar"></div>
-      <div class="bar"></div>
-      <div class="bar"></div>
-    </div>
-    <ul
-      :class="{
-        'nav-links-open animate__animated': isNavOpen,
-        animate__fadeInRight: isNavOpen,
-        animate__fadeOutRight: isClosing,
-      }"
-    >
-      <div class="btn-close" @click="closeNav"></div>
-      <li class="menu-mobile">
-        <router-link to="/" @click="closeNav">Home</router-link>
-      </li>
-      <li class="menu-mobile">
-        <router-link to="/about" @click="closeNav">Quem Somos</router-link>
-      </li>
-      <li class="menu-mobile">
-        <router-link to="/posts" @click="closeNav">Posts</router-link>
-      </li>
-    </ul>
-  </nav>
+  </div>
 </template>
 
 <script>
-import "animate.css";
+import Socials from "./Socials.vue";
 export default {
-  name: "Navbar",
-  data() {
-    return {
-      isNavOpen: false,
-      isClosing: false,
-    };
-  },
-  methods: {
-    toggleNav() {
-      this.isNavOpen = !this.isNavOpen;
-      this.isClosing = false;
-      this.toggleBodyScroll();
-    },
-    closeNav() {
-      if (this.isNavOpen) {
-        this.isClosing = true;
-
-        setTimeout(() => {
-          this.isNavOpen = false;
-          this.isClosing = false;
-          this.toggleBodyScroll();
-        }, 300);
-      }
-    },
-    toggleBodyScroll() {
-      if (this.isNavOpen) {
-        document.body.style.overflow = "hidden";
-        document.body.addEventListener("touchmove", this.preventScroll, {
-          passive: false,
-        });
-      } else {
-        document.body.style.overflow = "";
-        document.body.removeEventListener("touchmove", this.preventScroll, {
-          passive: false,
-        });
-      }
-    },
-    preventScroll(e) {
-      e.preventDefault();
-    },
+  name: "Footer",
+  components: {
+    Socials,
   },
 };
 </script>
-
 <style lang="scss" scoped>
-nav {
-  background-color: #808080;
-  color: var(--color-4);
-  padding: 1rem;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  color: var(--color-4);
-
-  .adopets {
+.container {
+  padding: 3rem 0rem;
+  background-color: var(--color-1);
+  .content {
+    padding: 0px 10px;
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
     .link {
+      width: 100%;
       display: flex;
-      gap: 1rem;
+      gap: 0.2rem;
       align-items: center;
+      justify-content: center;
       .logo {
         width: 60px;
       }
-    }
-  }
-
-  .navbar-toggle {
-    display: none;
-    flex-direction: column;
-    cursor: pointer;
-    .bar {
-      background-color: var(--color-4);
-      height: 2px;
-      width: 25px;
-      margin: 4px 0;
-      transition: transform 0.4s;
-    }
-  }
-
-  ul {
-    list-style: none;
-    display: flex;
-    .btn-close {
-      display: none;
-    }
-
-    li {
-      margin-right: 1rem;
-    }
-
-    &.nav-links-open {
-      flex-direction: column;
-      position: absolute;
-      width: 50%;
-      height: 100%;
-      top: 0px;
-      right: 0px;
-      background-color: var(--color-3);
-      box-shadow: -3px 0px 5px rgba($color: #000000, $alpha: 25);
-      padding: 1rem;
-      display: none;
-      z-index: 1;
-      li {
-        color: var(--color-4);
-        margin: 0;
-        margin-bottom: 1rem;
+      .title {
+        font-size: 24px;
       }
     }
-  }
-
-  @media (max-width: 768px) {
-    .navbar-toggle {
+    .socials {
+      width: 100%;
       display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+      align-items: center;
+      justify-content: center;
     }
-
-    ul {
-      display: none;
-
-      &.nav-links-open {
-        padding: 100px 0px;
-        display: flex;
-        .btn-close {
-          display: flex;
-          justify-content: flex-end;
-          align-items: center;
-          font-weight: 600;
-          padding: 10px;
-          background-color: transparent;
-          cursor: pointer;
-          transition: background-color 0.3s;
-
-          &::before {
-            content: "X";
-            font-size: 18px;
-            clip-path: circle();
-            color: var(--color-3);
-            background-color: var(--color-4);
-            padding: 5px;
-          }
-        }
-        .menu-mobile {
-          padding: 1rem;
-          text-align: center;
-        }
-      }
+    .copyright {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      font-size: 12px;
+      gap: 0.1rem;
     }
   }
 }

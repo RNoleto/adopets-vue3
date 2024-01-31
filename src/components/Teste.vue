e7cf90af6cb1d756c1af0a139254c1280b55d21e
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-01-31 18:29:43 -0300

    v1-api-conection
    conexao com api laravel para o projeto adopets

---
+++ src/components/Teste.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Animais Disponíveis para Adoção</h1>
+    <ul>
+      <li v-for="animal in animals" :key="animal.id">
+        {{ animal.name }} - {{ animal.breed }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      animals: [],
+    };
+  },
+  created() {
+    this.fetchAnimals();
+  },
+  methods: {
+    async fetchAnimals() {
+      try {
+        const response = await axios.get("http://localhost:8000/api/animals");
+        this.animals = response.data;
+      } catch (error) {
+        console.error("Erro ao buscar animais:", error);
+      }
+    },
+  },
+};
+</script>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,13 +4,16 @@
     <div class="content page">
       <h1>Página About</h1>
     </div>
+    <teste />
   </div>
 </template>
 <script>
 import Navbar from "../components/Navbar.vue";
+import Teste from "../components/Teste.vue";
 export default {
   components: {
     Navbar,
+    Teste,
   },
 };
 </script>
